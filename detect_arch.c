4a6a33bb3163311f6c43a16ada0b65c45d27b393
Lev Sizov <levsizov2015@outlook.com>
2017-05-27 23:30:17 +0700

    Fix: crucial part for architecture detection
    Signed-off-by: Lev Sizov <levsizov2015@outlook.com>

---
+++ detect_arch.c
@@ -0,0 +1,32 @@
+/*
+* MIT License
+*
+* Copyright (c) 2017 Lev Sizov a.k.a "ToxikCoder"
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+//This trick to find out system architecture is based on https://github.com/axr/solar-cmake/blob/master/TargetArch.cmake
+#if defined(i386) || defined(__i386) || defined(__i386__) || defined(_M_IX86) || defined(_X86_) || defined(__386) || defined(__X86__) || defined(__I86__)
+    #error cmake_ARCH i386
+#elif defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(__amd64__) || defined(_M_X64) || defined(_M_AMD64)
+    #error cmake_ARCH amd64
+#else
+    #error cmake_ARCH unknown
+#endif
